77d3dc3f28d1cea37eff7e0753bd92670fa5fbe9
Lakshay Bhatia <b.lakshay.aspect@gmail.com>
2026-04-28 18:59:14 +0530

    removed context file due to configuration incompatabilities

--- backend/query.py
+++ backend/query.py
@@ -1,13 +1,27 @@
-from context import retrieve_context
 from llm import generate_answer
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
+# Context Retrieval : 
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+vectorstore = FAISS.load_local(
+    "vectorstore",
+    embeddings,
+    allow_dangerous_deserialization=True
+)
+def retrieve_context(vectorstore,query, k=4):
+    docs = vectorstore.similarity_search(query, k=k)
+    return "\n\n".join([doc.page_content for doc in docs[:2]])
+
+
+# run
 if __name__ == "__main__":
     while True:
         query = input("\nAsk a legal question (type 'exit' to close): ")
         if query.lower() == "exit":
             break
 
-        context = retrieve_context(query)
+        context = retrieve_context(vectorstore,query)
         print("\n****Retrieved Context****\n")
         print(context)
         print("\n****End of Context****\n")
